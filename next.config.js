2e096274d2f973ed446f7ebe94a1a654ebcd9417
Zainab273 <rashidzainab990@gmail.com>
2024-12-10 00:58:43 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+const nextConfig = {
+  // Add your Next.js configuration options here
+  reactStrictMode: true, // Example option: Enables React's strict mode
+};
+
+module.exports = nextConfig;
